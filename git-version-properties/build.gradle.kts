76d210763db37319a1513334a42475196aa02188
Hans-Uwe Brackel <hbrackel@googlemail.com>
2024-01-10 21:50:46 +0100

    add strikt-core as test dependency

--- git-version-properties/build.gradle.kts
+++ git-version-properties/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.strikt:strikt-core:0.34.0")
 }
 
 gradlePlugin {
